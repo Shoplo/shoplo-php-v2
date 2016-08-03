f3bbdfe13e40933e724d119b800d84191b55ab4f
adrianadamiec <adrianadamiec@gmail.com>
2016-08-03 09:29:46 +0200

    remove product image

--- src/Shoplo/ProductImage.php
+++ src/Shoplo/ProductImage.php
@@ -4,4 +4,8 @@ namespace Shoplo;
 
 class ProductImage extends Resource
 {
+    public function remove($productId, $imageId)
+    {
+        return $this->send("products/" . $productId . "/images/" . $imageId, 'DELETE');
+    }
 }
